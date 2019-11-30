3f1df4025cf5468780edbf1db3f673d9b9172c8a
chebb44 <chebb@yandex.ru>
2019-11-30 11:34:58 +0400

    test4

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var formNewMsgText = formNewMsg.querySelector('.chat-form-input');
 var msgTemplate = document.querySelector('#message-template').content;
 var newMsg = msgTemplate.querySelector('.chat-message');
 var sendButton = formNewMsg.querySelector ('.chat-form-button');
-qqq1;
+qqq12;
 
 sendButton.addEventListener('click', function(evt){
   evt.preventDefault();
